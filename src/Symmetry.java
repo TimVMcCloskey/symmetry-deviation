2b2469bbd76bcce414c51fd28bedabe5482f3b5a
TimVMcCloskey <tim.v.mccloskey@gmail.com>
2018-04-22 21:46:45 -0600

    Update Symmetry.java

--- src/Symmetry.java
+++ src/Symmetry.java
@@ -16,14 +16,23 @@ public class Symmetry {
         Circle circle;
 
         GeoIo geoIo = new GeoIo();
+        
+        // Get fragment outline from svg file
         scatter = geoIo.getScatterPath(args[0]);
         
+        // Get circle from svg file
         circle = geoIo.getCircle(args[0]);
+        
+        // Get spokes at 10 degree intervals
         circle.getSpokes(10);
         
+        // Get the deviations of each spoke clipped with fragments
         scatter.getRatios(circle);
+            
+        // Plot it
         Plot plot = new Plot(800, 600, 7.5, circle, scatter);
         
+        // And write out each angle with deviation
         geoIo.writeDeviations(args[1], scatter);
     }   
 }
